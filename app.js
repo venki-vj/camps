7fb0166db3fdce98aae2cec8bb525de64fb42427
venki-vj <73542029+venki-vj@users.noreply.github.com>
2021-02-26 10:44:33 +0530

    Update app.js

--- app.js
+++ app.js
@@ -69,7 +69,7 @@ store.on("error", function (e) {
         saveUninitialized: true,
         cookie: {
             httpOnly: true,
-            // secure: true,
+            secure: true,
             expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
             maxAge: 1000 * 60 * 60 * 24 * 7
         }
